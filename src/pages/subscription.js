d227af98be8396402bb48aaf7775631d5ff3a0b9
BounebRayan <raynbouneb@gmail.com>
2023-07-24 18:35:32 +0100

    adding the Investor portal

--- src/pages/subscription.js
+++ src/pages/subscription.js
@@ -14,16 +14,16 @@ const Subscribe = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
        <div className="w-full max-w-md p-6 bg-white rounded-md shadow-md">
-        <h1 className="text-2xl font-semibold text-center">Join Our Mailing List</h1>
+        <h1 className="text-2xl font-semibold text-center text-black">Join Our Mailing List</h1>
         <p className=" text-gray-400 mb-4 text-center">Enter your Email to get notified of the latest Web3 deals</p>
       <Input
         type="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         placeholder="Enter your email"
-        className="w-full p-3 mb-2 border rounded"
+        className="w-full p-3 mb-2 border rounded border-gray-500 text-gray-500"
       />
-      <Button onClick={handleSubscribe}  className="w-full p-3 text-white bg-black border border-black rounded cursor-pointer ">Subscribe</Button>
+      <Button onClick={handleSubscribe}  className="w-full p-3 text-white bg-black border border-black rounded cursor-pointer hover:bg-black hover:text-white ">Subscribe</Button>
     </div>
     </div>
   );
