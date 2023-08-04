2a600418c2c7ef71c8646dcbb9ae873eb7327102
BounebRayan <raynbouneb@gmail.com>
2023-08-04 15:06:25 +0100

    Logout fix

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,11 +1,10 @@
 import { useSession, signOut } from "next-auth/react";
-import { useEffect,useState} from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from 'next/router'
 import DashboardHeader from "../components/DashboadHeader";
 import SidebarNav from "../components/SidebarNav";
 import ProductFeed from "../components/PostFeed";
-import { redirect } from "next/navigation"
 import SettingsForm from "../components/Settings";
 import BillingForm from "../components/Billing";
 
@@ -14,13 +13,13 @@ export default function Dashboard({ user }) {
   const router = useRouter()
   const [theme, setTheme] = useState(null);
 
-
   useEffect(() => {
     if (typeof window !== 'undefined') {
       setTheme(localStorage.getItem('theme'));
     }
   }, []);
 
+  // If the status of the session is still loading, show a loader
   if (status === "loading") {
     return (
       <div className="min-h-screen flex items-center justify-center">
@@ -29,16 +28,18 @@ export default function Dashboard({ user }) {
     );
   }
 
+  // If there's no session, redirect to /?week=31
   if (!session) {
-    redirect("/week=31");
+    router.push("/?week=31");
+    return null;  // This line is important as it stops rendering the rest of the component after redirection
   }
 
   return (
     <div >
       <DashboardHeader theme={theme} setTheme={setTheme}/>
       <div className="flex gap-4">
-      <SidebarNav />
-      {router.asPath === "/dashboard" ? <ProductFeed /> : router.asPath === "/dashboard?show=billing" ? <BillingForm />: <SettingsForm /> }
+        <SidebarNav />
+        {router.asPath === "/dashboard" ? <ProductFeed /> : router.asPath === "/dashboard?show=billing" ? <BillingForm />: <SettingsForm /> }
       </div>
     </div>
   );
